490ed2ad8307afda574d60199a39a42615ee7f17
lconners90 <lconners90@yahoo.com.au>
2012-09-01 05:45:19 +0000

    added player:useSkills()

--- profiles/default.lua
+++ profiles/default.lua
@@ -1,4 +1,8 @@
 return {
+	['loot'] = true, -- to loot mobs or not
+	['heal'] = 60, -- what % hp to use heal skill
+
+	--=== cooldown for the skills ===--
 	['skill2cd'] = 10,
 	['skill3cd'] = 10,
 	['skill4cd'] = 10,
@@ -9,6 +13,38 @@ return {
 	['skill9cd'] = 10,
 	['skill0cd'] = 10,
 	
-	['heal'] = 60, --what % hp to use heal skill
+	--=== which skills to use ===--
+	['skill2use'] = false,
+	['skill3use'] = false,
+	['skill4use'] = false,
+	['skill5use'] = false,
+	['skill6use'] = true,
+	['skill7use'] = false,
+	['skill8use'] = false,
+	['skill9use'] = false,
+	['skill0use'] = false,
+
+	--=== if the skill needs to target ground ===--
+	['skill2ground'] = false,
+	['skill3ground'] = false,
+	['skill4ground'] = false,
+	['skill5ground'] = false,
+	['skill6ground'] = false,
+	['skill7ground'] = false,
+	['skill8ground'] = false,
+	['skill9ground'] = false,
+	['skill0ground'] = false,	
+	
+	--=== cast time of the skills ===--
+	['skill2casttime'] = 1,
+	['skill3casttime'] = 1,
+	['skill4casttime'] = 1,
+	['skill5casttime'] = 1,
+	['skill6casttime'] = 1,
+	['skill7casttime'] = 1,
+	['skill8casttime'] = 1,
+	['skill9casttime'] = 1,
+	['skill0casttime'] = 1,
+
 	
 }
