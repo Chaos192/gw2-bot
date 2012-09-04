7796d6cd4e2ebfa30b93f421c5d3e369b8e43ca5
lconners90@yahoo.com.au <lconners90@yahoo.com.au@a63c1098-83f3-de66-ba72-a8f4ffc24999>
2012-09-04 11:46:27 +0000

    mainly just adding the commit to another PC =)

---
+++ language/french.lua
@@ -0,0 +1,15 @@
+return {
+	['test'] = "Test language\n",
+	['WPinsert'] = "%s: Insert new waypoint\n",
+	['WPharvest'] = "%s: Insert new harvest waypoint\n",
+	['WPsaveend'] = "%s: Save waypoints and quit\n",
+	['WPcommand'] = "%s: Insert merchant command\n",
+	['WPnpc'] = "%s: Insert target/dialog NPC command\n",
+	['WPchoice'] = "%s: Insert choiceOption command\n",
+	['WPmouse'] = "%s: Insert Mouseclick Left command\n",
+	['WPreset'] = "%s: Reset script\n",
+	['WPsavenew'] = "%s: Save waypoints and restart\n",
+	['WPcode'] = "%s: Insert code command\n",
+	['WPobj'] = "%s: Insert target obj command\n",
+	['WPcontinue'] = "Continue to next. Press %s to save and quit\n",
+}
