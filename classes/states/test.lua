b6392ab4a622c5f0f1c0b38875a3fc460a1eec83
d003232@gmail.com <d003232@gmail.com@a63c1098-83f3-de66-ba72-a8f4ffc24999>
2012-09-13 07:02:02 +0000

    add test case for moving problem

---
+++ classes/states/test.lua
@@ -0,0 +1,43 @@
+--[[
+	Child of State class.
+]]
+
+include("../state.lua");
+
+TestState = class(State);
+
+function TestState:constructor()
+	self.name = "Test";
+	self.MoveFinished = false;		
+end
+
+
+function TestState:update()
+	logger:log('debug',"Coming to TestState:update()");
+
+	if SETTINGS['combatstate'] == true then SETTINGS['combatstate'] = false end -- stops combat being pushed
+
+		if (self.MoveFinished == true ) then
+			logger:log('debug',"MoveFinished = true (nothing more to do => return to main");
+--			error("Move finished")
+			return
+		end
+
+
+		if not player:moveTo_step(-26618, 10294, 100 ) then
+			logger:log('debug',"move not finished: distance %d", distance(player.X, player.Z, -26618, 10294) );
+			self.MoveFinished = false;
+		else
+			logger:log('info',"*** move finished ***");
+			self.MoveFinished = true;
+		end
+
+end
+
+
+-- Handle events
+function TestState:handleEvent(event)
+
+end
+
+table.insert(events,{name = "Test", func = TestState()})
