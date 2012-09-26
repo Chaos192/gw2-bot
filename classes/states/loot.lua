bf5aad1a935abc80049f2ce9d2937605b7c100fd
d003232@gmail.com <d003232@gmail.com@a63c1098-83f3-de66-ba72-a8f4ffc24999>
2012-09-26 21:31:56 +0000

    change: use random waitrun/harvestruns between bridge event waiting time fix: combat.lua use targetupdate() to avoid fighting dead targets fix: loot.lua don't clear target while looting fix: waypoint.lua wait 4 seconds to finish harvesting

--- classes/states/loot.lua
+++ classes/states/loot.lua
@@ -12,10 +12,12 @@ end
 
 function LootState:update()
 	logger:log('debug-states',"Coming to LootState:update()");
-	if player.TargetMob ~= 0 then
-		keyboardPress(key.VK_ESCAPE)
-		yrest(1000)
-	end
+
+-- Why clear target? We can also loot during having a target
+--	if player.TargetMob ~= 0 then
+--		keyboardPress(key.VK_ESCAPE)
+--		yrest(1000)
+--	end
 
 	if( player.Interaction ) then
 --	    player.InteractionId == 0x1403F ) then		-- FIX not working ATM
